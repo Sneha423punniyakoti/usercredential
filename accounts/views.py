8d718a82b2faeb7caf263c3bdeaff1be057d3e82
Sneha423punniyakoti <punniyakotisneha@gmail.com>
2025-04-30 17:15:28 +0530

    initial commit

---
+++ accounts/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        messages.success(request, 'Account created successfully!')
+        return redirect('login')
+    return render(request, 'register.html')
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# Create your views here.
